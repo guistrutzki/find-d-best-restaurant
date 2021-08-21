202fbcca56a2745774fce4715cd2fab33d882dbd
Pedro Neto <pedro.santosnt@hotmail.com>
2021-08-21 03:52:52 -0300

    Adiciona label do titulo e subtítulo a tela inicial

--- FindDBestRestaurant/HomeViewController.swift
+++ FindDBestRestaurant/HomeViewController.swift
@@ -9,11 +9,59 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "FIND D BEST"
+        label.font = UIFont.systemFont(ofSize: 40)
+        label.textAlignment = .center
+        label.textColor = .white
+        
+        return label
+    }()
+    
+    lazy var subTitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Restaurant"
+        label.font = UIFont.systemFont(ofSize: 24)
+        label.textAlignment = .center
+        label.textColor = .white
+        
+        return label
+    }()
+    
+    override func loadView() {
+        let view = UIView(frame: UIScreen.main.bounds)
+        view.backgroundColor = .black
+        self.view = view
+        
+        addSubViewsToView()
+        addSubviewConstraints()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        navigationController?.isNavigationBarHidden = false
-        title = "Home"
-        view.backgroundColor = .blue
+        title = "Main View Controller"
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    func addSubViewsToView() {
+        
+        view.addSubview(titleLabel)
+        view.addSubview(subTitleLabel)
+    }
+    
+    func addSubviewConstraints() {
+        NSLayoutConstraint.activate([
+
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            subTitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor),
+            subTitleLabel.centerXAnchor.constraint(equalTo: titleLabel.centerXAnchor),
+            
+        ])
     }
 }
