c7aa555103efe5a2f5335cb11fcf94aaebbfefe1
Pedro Neto <pedro.santosnt@hotmail.com>
2021-08-21 03:55:04 -0300

    Adiciona targets aos botões da tela inicial

--- FindDBestRestaurant/HomeViewController.swift
+++ FindDBestRestaurant/HomeViewController.swift
@@ -32,39 +32,42 @@ class HomeViewController: UIViewController {
     }()
     
     lazy var loginButton: UIButton = {
-        let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("Entrar", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 20)
-        button.layer.backgroundColor = UIColor(red: 0.904, green: 0.188, blue: 0.188, alpha: 1).cgColor
+         let button = UIButton()
+         button.translatesAutoresizingMaskIntoConstraints = false
+         button.setTitle("Entrar", for: .normal)
+         button.setTitleColor(.white, for: .normal)
+         button.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+         button.layer.backgroundColor = UIColor(red: 0.904, green: 0.188, blue: 0.188, alpha: 1).cgColor
+         button.addTarget(self, action: #selector(loginButtonPressed), for: .touchUpInside)
 
-        button.layer.cornerRadius = 8
-        return button
-    }()
-    
-    lazy var registerButton: UIButton = {
-        let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("Cadastrar", for: .normal)
-        button.setTitleColor(UIColor(red: 0.906, green: 0.188, blue: 0.188, alpha: 1), for: .normal)
-        button.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 20)
-        button.layer.cornerRadius = 8
-        button.backgroundColor = .white
+         button.layer.cornerRadius = 8
+         return button
+     }()
+     
+     lazy var registerButton: UIButton = {
+         let button = UIButton()
+         button.translatesAutoresizingMaskIntoConstraints = false
+         button.setTitle("Cadastrar", for: .normal)
+         button.setTitleColor(UIColor(red: 0.906, green: 0.188, blue: 0.188, alpha: 1), for: .normal)
+         button.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+         button.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+         button.layer.cornerRadius = 8
+         button.backgroundColor = .white
+         button.addTarget(self, action: #selector(registerButtonPressed), for: .touchUpInside)
 
-        return button
-    }()
-    
-    lazy var continueWithoutLoginButton: UIButton = {
-        let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("Continuar sem login  >", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 20)
-        
-        return button
-    }()
+         return button
+     }()
+     
+     lazy var continueWithoutLoginButton: UIButton = {
+         let button = UIButton()
+         button.translatesAutoresizingMaskIntoConstraints = false
+         button.setTitle("Continuar sem login  >", for: .normal)
+         button.setTitleColor(.white, for: .normal)
+         button.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+         button.addTarget(self, action: #selector(continueWithoutLoginButtonPressed), for: .touchUpInside)
+         
+         return button
+     }()
     
     override func loadView() {
         let view = UIView(frame: UIScreen.main.bounds)
@@ -90,6 +93,21 @@ class HomeViewController: UIViewController {
         view.addSubview(loginButton)
         view.addSubview(registerButton)
         view.addSubview(continueWithoutLoginButton)
+    }
+    
+    @objc func loginButtonPressed() {
+        let vc  = LoginViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @objc func registerButtonPressed() {
+
+    }
+    
+    @objc func continueWithoutLoginButtonPressed() {
+        let vc = MainViewController()
+        navigationController?.pushViewController(vc, animated: true)
+
     }
     
     func addSubviewConstraints() {
