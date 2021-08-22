c5e146f26f63e5383eaf17d4b56ef8ddf4ec4ec0
Alexandre Cardoso <developer.ios.alexandre@gmail.com>
2021-08-22 12:12:42 -0300

    Implementado protocol CodeView e CustomButtonView.

---
+++ FindDBestRestaurant/Helper/CodeView.swift
@@ -0,0 +1,24 @@
+//
+//  CodeView.swift
+//  FindDBestRestaurant
+//
+//  Created by Alexandre Cardoso on 22/08/21.
+//
+
+import Foundation
+import SnapKit
+
+protocol CodeView {
+	func buildViewHierarchy()
+	func setupConstraints()
+	func setupAdditionalConfiguration()
+	func setupView()
+}
+
+extension CodeView {
+	func setupView() {
+		buildViewHierarchy()
+		setupConstraints()
+		setupAdditionalConfiguration()
+	}
+}
