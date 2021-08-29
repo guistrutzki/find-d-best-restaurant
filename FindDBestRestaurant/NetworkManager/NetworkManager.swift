d238fa5c2f32a181a2a2f214743d15cc6b67601b
Bruna Drago <bruna_drago@hotmail.com>
2021-08-29 20:35:57 -0300

    [FBR] networkManager

--- FindDBestRestaurant/NetworkManager/NetworkManager.swift
+++ FindDBestRestaurant/NetworkManager/NetworkManager.swift
@@ -6,3 +6,30 @@
 //
 
 import Alamofire
+
+typealias NetworkResult = ((Result<RestaurantListResponse, Error>) -> Void)
+
+class NetworkManager {
+    
+    private init() {}
+    
+    static func request(url: String, completion: @escaping NetworkResult) {
+        let request = AF.request(url, method: .get)
+        
+        request.validate().responseJSON { response in
+            switch response.result {
+            case .success:
+                do {
+                    let data = response.data ?? Data()
+                    let result = try JSONDecoder().decode(RestaurantListResponse.self, from: data)
+                    
+                    completion(.success(result))
+                } catch {
+                    completion(.failure(error))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
