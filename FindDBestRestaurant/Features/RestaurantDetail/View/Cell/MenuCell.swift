7c7b51208983e7b298531872a82c1c6ef73ff4b1
Bruna Drago <bruna_drago@hotmail.com>
2021-09-01 21:39:56 -0300

    [FBR]add menuCell

---
+++ FindDBestRestaurant/Features/RestaurantDetail/View/Cell/MenuCell.swift
@@ -0,0 +1,13 @@
+//
+//  MenuCell.swift
+//  FindDBestRestaurant
+//
+//  Created by Bruna Drago on 01/09/21.
+//
+
+import UIKit
+
+class MenuCell: UICollectionViewCell {
+    
+    static let identifier = String(describing: MenuCell.self)
+}
