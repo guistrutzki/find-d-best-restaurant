6afaa14e7712517cb3afb8ccd666a969ad28c8ad
Alexandre Cardoso <developer.ios.alexandre@gmail.com>
2021-09-09 21:32:22 -0300

    finalizado Top Restaurantes e card dos restaurantes.

--- FindDBestRestaurant/Features/TopRestaurants/View/Screens/TopRestaurantsScreen.swift
+++ FindDBestRestaurant/Features/TopRestaurants/View/Screens/TopRestaurantsScreen.swift
@@ -8,33 +8,56 @@
 import UIKit
 import MapKit
 
+protocol TopRestaurantsScreenDelegate: AnyObject {
+	func didTappedLocation()
+}
+
 class TopRestaurantsScreen: UIView {
 	
 	// MARK: - UI Elements
 	private lazy var mapView: MKMapView = {
 		let map = MKMapView(frame: .zero)
 		map.overrideUserInterfaceStyle = .dark
-		map.translatesAutoresizingMaskIntoConstraints = false
 		return map
+	}()
+	
+	private lazy var locationView: UIView = {
+		let view = UIView(frame: .zero)
+		view.backgroundColor = Colors.gray800
+		view.layer.cornerRadius = 12
+		let tap = UITapGestureRecognizer(target: self, action: #selector(didTappedLocation(_:)))
+		view.addGestureRecognizer(tap)
+		return view
+	}()
+	
+	private var locationImageView: UIImageView = {
+		let imageView = UIImageView(frame: .zero)
+		imageView.image = UIImage(systemName: "location")
+		imageView.tintColor = .white
+		return imageView
 	}()
 	
 	private lazy var collectionView: UICollectionView = {
 		let layout = UICollectionViewFlowLayout()
 		layout.scrollDirection = .horizontal
 		let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
-		collection.translatesAutoresizingMaskIntoConstraints = false
-		collection.translatesAutoresizingMaskIntoConstraints = false
 		collection.backgroundColor = .clear
-//		collection.register(CardCollectionViewCell.self, forCellWithReuseIdentifier: CardCollectionViewCell.identifier)
 		collection.contentInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
 		collection.showsHorizontalScrollIndicator = false
 		return collection
 	}()
+	
+	private weak var delegate: TopRestaurantsScreenDelegate?
 		
 	// MARK: - Initialize
+	init(delegate: TopRestaurantsScreenDelegate?) {
+		super.init(frame: .zero)
+		self.delegate = delegate
+		setupView()
+	}
+	
 	override init(frame: CGRect) {
 		super.init(frame: frame)
-		
 		setupView()
 	}
 	
@@ -53,6 +76,49 @@ class TopRestaurantsScreen: UIView {
 		self.collectionView.dataSource = dataSource
 	}
 	
+	func reloadData() {
+		self.collectionView.reloadData()
+	}
+	
+	func showsUserLocation() {
+		mapView.showsUserLocation = true
+	}
+	
+	func centerViewOnUserLocation(_ locationManager: CLLocationManager) {
+		if let location = locationManager.location?.coordinate {
+			let region = MKCoordinateRegion.init(center: location, latitudinalMeters: 5000,
+															 longitudinalMeters: 5000)
+			mapView.setRegion(region, animated: true)
+		}
+	}
+	
+	func centerViewOnRestaurant(_ restaurant: RestaurantListResponse) {
+		let latitude = CLLocationDegrees(restaurant.lat) ?? 0
+		let longitude = CLLocationDegrees(restaurant.lng) ?? 0
+		let center = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+		let region = MKCoordinateRegion.init(center: center, latitudinalMeters: 100,
+														 longitudinalMeters: 100)
+		
+		mapView.setRegion(region, animated: true)
+	}
+	
+	func addAnnotation(restaurant: RestaurantListResponse) {
+		let annotation = MKPointAnnotation()
+		let latitude = CLLocationDegrees(restaurant.lat) ?? 0
+		let longitude = CLLocationDegrees(restaurant.lng) ?? 0
+		let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+		annotation.coordinate = coordinate
+		annotation.title = restaurant.name
+		annotation.subtitle = restaurant.description
+		
+		mapView.addAnnotation(annotation)
+	}
+	
+	@objc
+	func didTappedLocation(_ sender: UITapGestureRecognizer) {
+		self.delegate?.didTappedLocation()
+	}
+	
 }
 
 // MARK: - Extension CodeView
@@ -61,36 +127,42 @@ extension TopRestaurantsScreen: CodeView {
 	func buildViewHierarchy() {
 		addSubview(mapView)
 		addSubview(collectionView)
+		addSubview(locationView)
+		locationView.addSubview(locationImageView)
 	}
 	
 	func setupConstraints() {
+		mapView.snp.makeConstraints { make in
+			make.top.equalTo(safeAreaLayoutGuide.snp.top)
+			make.left.right.equalToSuperview()
+			make.bottom.equalTo(safeAreaLayoutGuide.snp.bottom)
+		}
 		
-		NSLayoutConstraint.activate([
-			
-			mapView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
-			mapView.leadingAnchor.constraint(equalTo: leadingAnchor),
-			mapView.trailingAnchor.constraint(equalTo: trailingAnchor),
-			mapView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
-
-			collectionView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
-			collectionView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
-			collectionView.bottomAnchor.constraint(equalTo: mapView.bottomAnchor, constant: -40),
-			collectionView.heightAnchor.constraint(equalToConstant: 120)
-		])
+		collectionView.snp.makeConstraints { make in
+			make.left.right.equalToSuperview()
+			make.bottom.equalTo(mapView.snp.bottom).inset(10)
+			make.height.equalTo(110)
+		}
 		
-//		mapView.snp.makeConstraints { make in
-//			make.top.left.right.bottom.equalToSuperview()
-//		}
+		locationView.snp.makeConstraints { make in
+			make.top.equalTo(mapView.snp.top).offset(20)
+			make.right.equalTo(mapView.snp.right)
+			make.height.equalTo(60)
+			make.width.equalTo(50)
+		}
 		
-//		collectionView.snp.makeConstraints { make in
-//			make.left.right.equalToSuperview()
-//			make.bottom.equalTo(mapView.snp.bottom).inset(40)
-//			make.height.equalTo(120)
-//		}
+		locationImageView.snp.makeConstraints { make in
+			make.top.equalTo(locationView.snp.top).offset(15)
+			make.left.equalTo(locationView.snp.left).offset(10)
+			make.right.equalTo(locationView.snp.right).inset(10)
+			make.bottom.equalTo(locationView.snp.bottom).inset(15)
+		}
+		
 	}
 	
 	func setupAdditionalConfiguration() {
 		backgroundColor = Colors.gray800
+		TopRestaurantsCollectionCell.registerOn(collectionView)
 	}
 	
 }
--- FindDBestRestaurant/Features/TopRestaurants/View/TopRestaurantsViewController.swift
+++ FindDBestRestaurant/Features/TopRestaurants/View/TopRestaurantsViewController.swift
@@ -8,27 +8,75 @@
 import UIKit
 import MapKit
 
+
 class TopRestaurantsViewController: UIViewController {
 	
 	// MARK: - Elements
 	private lazy var topRestaurantScreen: TopRestaurantsScreen = {
-		let screen = TopRestaurantsScreen()
+		let screen = TopRestaurantsScreen(delegate: self)
 		screen.configureDelegateMapKit(delegate: self)
 		screen.configureDelegateCollectionView(delegate: self, dataSource: self)
 		return screen
 	}()
+	private lazy var controller: TopRestaurantsController = {
+		let controller = TopRestaurantsController()
+		controller.delegate = self
+		return controller
+	}()
+	
+	private let locationManager = CLLocationManager()
 	
 	// MARK: - Life cycle
 	override func loadView() {
 		self.view = topRestaurantScreen
 	}
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-		navigationController?.isNavigationBarHidden = true
-    }
 	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		navigationController?.isNavigationBarHidden = true
+		checkLocationManager()
+	}
+	
+	override func viewWillAppear(_ animated: Bool) {
+		controller.loadRestaurantList()
+		self.topRestaurantScreen.centerViewOnUserLocation(locationManager)
+	}
+	
+	// MARK: - Function
+	private func setupLocationManager() {
+		locationManager.delegate = self
+		locationManager.desiredAccuracy = kCLLocationAccuracyBest
+	}
+	
+	private func checkLocationManager() {
+		if CLLocationManager.locationServicesEnabled() {
+			setupLocationManager()
+			checkLocationAuthorization()
+		}
+	}
+	
+	private func checkLocationAuthorization() {
+		switch locationManager.authorizationStatus {
+			case .authorizedWhenInUse:
+				self.topRestaurantScreen.showsUserLocation()
+				self.topRestaurantScreen.centerViewOnUserLocation(locationManager)
+				//				locationManager.startUpdatingLocation()
+				break
+			case .denied:
+				break
+			case .notDetermined:
+				locationManager.requestWhenInUseAuthorization()
+			default:
+				break
+		}
+	}
+	
+	private func createAnnotationRestaurant(_ listRestaurant: [RestaurantListResponse]) {
+		listRestaurant.forEach { restaurant in
+			self.topRestaurantScreen.addAnnotation(restaurant: restaurant)
+		}
+	}
 	
 }
 
@@ -38,17 +86,30 @@ extension TopRestaurantsViewController: MKMapViewDelegate {
 
 // MARK: - Extension UICollectionViewDelegate
 extension TopRestaurantsViewController: UICollectionViewDelegate {
+	
+	func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+		let restaurant = controller.getRestaurants(indexPath: indexPath)
+		self.topRestaurantScreen.centerViewOnRestaurant(restaurant)
+	}
+	
 }
 
 // MARK: - Extension UICollectionViewDataSource
 extension TopRestaurantsViewController: UICollectionViewDataSource {
 	
 	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-		return 10
+		return controller.count
 	}
 	
 	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-		return UICollectionViewCell()
+		let restaurant = controller.getRestaurants(indexPath: indexPath)
+		guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TopRestaurantsCollectionCell.identifier,
+																			 for: indexPath) as? TopRestaurantsCollectionCell
+		else { return UICollectionViewCell() }
+		
+		cell.setupCell(restaurant: restaurant)
+		
+		return cell
 	}
 	
 }
@@ -57,7 +118,43 @@ extension TopRestaurantsViewController: UICollectionViewDataSource {
 extension TopRestaurantsViewController: UICollectionViewDelegateFlowLayout {
 	
 	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-		return CGSize(width: 250, height: 110)
+		return CGSize(width: 300, height: 110)
+	}
+	
+}
+
+// MARK: - Extension CLLocationManagerDelegate
+extension TopRestaurantsViewController: CLLocationManagerDelegate {
+	
+	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+		//		self.topRestaurantScreen.centerViewOnUserLocation(manager)
+	}
+	
+	func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+		checkLocationAuthorization()
+	}
+	
+}
+
+// MARK: - Extension TopRestaurantsControllerDelegate
+extension TopRestaurantsViewController: TopRestaurantsControllerDelegate {
+	
+	func successLoad(_ list: [RestaurantListResponse]) {
+		createAnnotationRestaurant(list)
+		self.topRestaurantScreen.reloadData()
+	}
+	
+	func errorLoad() {
+		print(#function)
+	}
+	
+}
+
+// MARK: - Extension TopRestaurantsScreen
+extension TopRestaurantsViewController: TopRestaurantsScreenDelegate {
+	
+	func didTappedLocation() {
+		self.topRestaurantScreen.centerViewOnUserLocation(locationManager)
 	}
 	
 }
