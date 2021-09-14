56ff8e1d0546873cbdc5b6f981ea50f9557e4a6f
Guilherme Strutzki <guilhermestrutzk@hotmail.com>
2021-09-14 18:49:53 -0300

    WIP: add sign in integration

--- FindDBestRestaurant/Features/SignUp/Screens/SignUpScreen.swift
+++ FindDBestRestaurant/Features/SignUp/Screens/SignUpScreen.swift
@@ -11,6 +11,23 @@ final class SignUpScreen: UIView {
     
     // MARK: - UI Element
     
+    lazy var backButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(named: "arrow-left"), for: .normal)
+        button.tintColor = .white
+        return button
+    }()
+    
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        label.text = K.signUp
+        return label
+    }()
+    
     private lazy var stackView: UIStackView = {
         let stack = UIStackView(frame: .zero)
         stack.axis = .vertical
@@ -88,6 +105,10 @@ final class SignUpScreen: UIView {
     private func didTappedSignUpButton(_ sender: Any) {
         delegate.didTappedSignUpButton()
     }
+    
+    @objc private func didTappedBackButton(_ button: UIButton) {
+        delegate.didTappedBackButton()
+    }
 }
 
 // MARK: - Extension UITextFieldDelegate
@@ -109,6 +130,8 @@ extension SignUpScreen: UITextFieldDelegate {
 extension SignUpScreen: CodeView {
     
     func buildViewHierarchy() {
+        addSubview(backButton)
+        addSubview(titleLabel)
         addSubview(stackView)
         stackView.addArrangedSubview(nameTextField)
         stackView.addArrangedSubview(emailTextField)
@@ -117,8 +140,20 @@ extension SignUpScreen: CodeView {
     }
     
     func setupConstraints() {
+        backButton.snp.makeConstraints { make in
+            make.top.equalTo(safeAreaLayoutGuide.snp.top).offset(20)
+            make.leading.equalToSuperview().offset(26)
+            make.height.equalTo(24)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(backButton.snp.centerY)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(24)
+        }
+        
         stackView.snp.makeConstraints { make in
-            make.top.equalTo(safeAreaLayoutGuide).offset(20)
+            make.top.equalTo(titleLabel.snp.bottom).offset(40)
             make.left.equalToSuperview().offset(20)
             make.right.equalToSuperview().inset(20)
         }
@@ -144,9 +179,12 @@ extension SignUpScreen: CodeView {
     }
     
     func setupAdditionalConfiguration() {
-        backgroundColor = .darkGray
+        backgroundColor = Colors.gray500
         
+        let backButtonAction = #selector(didTappedBackButton(_:))
         let signUpButtonAction = #selector( didTappedSignUpButton(_:))
+        
+        backButton.addTarget(self, action: backButtonAction, for: .touchUpInside)
         signUpButton.addTarget(self, action: signUpButtonAction, for: .touchUpInside)
     }
 }
--- FindDBestRestaurant/Features/SignUp/SignUpVC.swift
+++ FindDBestRestaurant/Features/SignUp/SignUpVC.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class SignUpVC: UIViewController {
     
+    let controller = SignUpController()
+    
     // MARK: - UI Element
     
     lazy var signUpScreen: SignUpScreen = {
@@ -27,6 +29,16 @@ class SignUpVC: UIViewController {
         configureView()
     }
     
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        .lightContent
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setNeedsStatusBarAppearanceUpdate()
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    
     // MARK: - Private Functions
     
     private func configureView() {
@@ -38,14 +50,34 @@ class SignUpVC: UIViewController {
             title: K.accountCreated,
             message: K.accountSuccessMessage)
     }
+    
+    private func didCreateAccountFailed() {
+        showMessage(
+            title: "Algo deu errado",
+            message: "Tente novamente")
+    }
 }
 
 // MARK: - Extension SignUpScreenDelegate
 
 extension SignUpVC: SignUpScreenDelegate {
     
+    func didTappedBackButton() {
+        navigationController?.popToRootViewController(animated: true)
+    }
+    
     func didTappedSignUpButton() {
-        didCreateAccountWithSuccess()
-        #warning("Chamar a Controller aqui pra fazer a ação")
+        let name = signUpScreen.getUserName()
+        let email = signUpScreen.getUserEmail()
+        let password = signUpScreen.getUserPassword()
+        
+        controller.createNewAccount(name: name, email: email, password: password) { isSuccess in
+            if isSuccess {
+                self.didCreateAccountWithSuccess()
+            } else {
+                self.didCreateAccountFailed()
+            }
+        }
+        
     }
 }
