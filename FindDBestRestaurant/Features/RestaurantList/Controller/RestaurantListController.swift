904a30198b66102f95b98721f777f2e9e1f9c029
Alexandre Cardoso <developer.ios.alexandre@gmail.com>
2021-11-02 14:54:29 -0300

    add favorite realm detail

--- FindDBestRestaurant/Features/RestaurantList/Controller/RestaurantListController.swift
+++ FindDBestRestaurant/Features/RestaurantList/Controller/RestaurantListController.swift
@@ -23,9 +23,10 @@ class RestaurantListController {
       if filterdRestaurants.isEmpty {
          return restaurants.count
       }
-      
-        return filterdRestaurants.count
+		return filterdRestaurants.count
     }
+	
+	 let favoriteRealmManager = FavoriteRealmManager()
     
     weak var delegate: RestaurantListControllerDelegate?
     
@@ -85,6 +86,33 @@ class RestaurantListController {
 			return
 		}
 		filterdRestaurants = restaurants.filter { $0.name.lowercased().contains(searchText.lowercased()) }
+	}
+	
+	func loadFavorites() {
+		favoriteRealmManager.loadFavorites()
+	}
+	
+	func isFavoriteRestaurant(_ restaurant: RestaurantListResponse) -> Bool {
+		guard let result = favoriteRealmManager.getFavorite(restaurant)
+		else { return false }
+		
+		if result.isEmpty {
+			return false
+		} else {
+			return true
+		}
+	}
+	
+	func addRestaurantToFavorite(_ restaurant: RestaurantListResponse) {
+		let favorite = FavoriteRealm()
+		favorite.idRestaurant = restaurant.id
+		favoriteRealmManager.add(newFavorite: favorite)
+	}
+	
+	func removeRestaurantToFavorite(_ restaurant: RestaurantListResponse) {
+		let favorite = FavoriteRealm()
+		favorite.idRestaurant = restaurant.id
+		favoriteRealmManager.delete(favorite)
 	}
     
     // MARK: - Private Functions
